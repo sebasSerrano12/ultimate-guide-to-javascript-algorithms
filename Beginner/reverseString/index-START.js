ffab9f4b6e2e07f03c62b4732bd47f2bf84dcd62
Sebastian Serrano <se.serrano12@gmail.com>
2025-05-16 13:52:45 +0000

    changes to add code solutions

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,8 +6,35 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+    //Chaining in-built methods
+
+    //return text.split('').reverse().join('')
+    //return[...text].reverse().join('')
+
+    //For-loop way
+    // let result =''
+    // for (let i=text.length - 1; i >= 0; i--){
+    //     result += text[i]
+    // }
+
+    // let result ='';
+    // for (let char of text){
+    //     result = char+result
+    // }
+    // return result
+
+    //Recursive way
+    // if (text ===''){
+    // return ''
+    // }else {
+    //     return reverseString(text.substr(1)) + text[0]
+    // }
+
+    //Using reduce()
+    //return text.split('').reduce((a,c) => c + a, '')
+    return [...text].reduce((a,c) => c + a,'')
 }
+
 
 
 
