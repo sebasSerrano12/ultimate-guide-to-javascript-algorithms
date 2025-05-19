b805dec7432141518441edc83d4979c1e4bacfa8
Sebastian Serrano <se.serrano12@gmail.com>
2025-05-19 14:51:54 +0000

    added new code to file

--- Beginner/maxRecurringChar/index-START.js
+++ Beginner/maxRecurringChar/index-START.js
@@ -7,6 +7,43 @@ e.g maxRecurringChar('aabacada') // will return 'a'
 
 function maxRecurringChar(text) {
     // Code goes here
+    // let charMap ={}
+    // let maxCharValue = 0
+    // let maxChar =''
+
+    // for (let char of text){
+    //     if (charMap.hasOwnProperty(char)){
+    //         charMap[char]++
+    //     } else {
+    //         charMap[char] = 1
+    //     }
+    // }
+
+    // for (let char in charMap){
+    //     if (charMap[char] > maxCharValue){
+    //         maxCharValue = charMap[char]
+    //         maxChar = char
+    //     }
+    // }
+    // return maxChar
+
+    let charMap ={}
+    let charArray = []
+    let valuesArray =[]
+    let maxCharValue = 0
+    
+    for (let char of text){
+        if (charMap.hasOwnProperty(char)){
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+    charArray = Object.keys(charMap)
+    valuesArray = Object.values(charMap)
+    maxCharValue = Math.max(...valuesArray)
+
+    return charArray[valuesArray.indexOf(maxCharValue)]
 }
 
 
