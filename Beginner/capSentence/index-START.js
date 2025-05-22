bb0c7b5d2dfa84a8b14d8e939b22c17e71686c1d
Sebastian Serrano <se.serrano12@gmail.com>
2025-05-22 12:55:59 +0000

    completed problem

--- Beginner/capSentence/index-START.js
+++ Beginner/capSentence/index-START.js
@@ -7,9 +7,31 @@ return the equivalent of the sentence when capitalised. E.g
 
 
 
-function capSentence(text) {
-   // Code goes here
+function capSentence(text){
+ // .forEach() Solution
+  //   let wordsArray = text.toLowerCase().split(' ')
+  //   let capsArray = []
+  //   wordsArray.forEach(word=> {
+  //     capsArray.push(word[0].toUpperCase() + word.slice(1))
+  //   })
+  //   return capsArray.join(' ')
+  
+ // .map() & .slice() Solution (Best solution)
+    let wordsArray = text.toLowerCase().split(' ')
+    let capsArray = wordsArray.map(word =>{
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    return capsArray.join(' ')
+  
+ // .map() & .replace() Solution (Slowest)
+  // let wordsArray = text.toLowerCase().split(' ')
+  // let capsArray = wordsArray.map(word =>{
+  //   return word.replace(word[0],word[0].toUpperCase())
+  // })
+  // return capsArray.join(' ')
 }
+
+console.log(capSentence('what is up !!'))
 
 
 
